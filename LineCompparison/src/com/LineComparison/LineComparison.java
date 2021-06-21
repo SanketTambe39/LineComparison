ca7d4056f1056a86441b16c4470db410c4b515c0
SanketTambe39 <sankettambe39@gmail.com>
2021-06-21 23:49:20 +0530

    Added: Distance formula

--- LineCompparison/src/com/LineComparison/LineComparison.java
+++ LineCompparison/src/com/LineComparison/LineComparison.java
@@ -4,8 +4,12 @@ public class LineComparison {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to line Comparison Computation Program on Master Branch");
+		int x1=3,x2=5,y1=2,y2=6;
+		
+		double LENGTH_OF_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
 
+		System.out.println("From the given input distance between these two lines is ::"+LENGTH_OF_LINE);
+		
 	}
 
 }
