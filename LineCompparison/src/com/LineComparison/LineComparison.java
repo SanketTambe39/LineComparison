86df78b207d800d4f0586f5fead49cf4719ee16d
SanketTambe39 <sankettambe39@gmail.com>
2021-06-22 00:05:35 +0530

    Added : Comparison

--- LineCompparison/src/com/LineComparison/LineComparison.java
+++ LineCompparison/src/com/LineComparison/LineComparison.java
@@ -6,10 +6,21 @@ public class LineComparison {
 		
 		int x1=3,x2=5,y1=2,y2=6;
 		
-		double LENGTH_OF_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+		double LENGTH_OF_FIRST_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
 
-		System.out.println("From the given input distance between these two lines is ::"+LENGTH_OF_LINE);
+		int x3=3,x4=5,y3=2,y4=6;
 		
+		double LENGTH_OF_SECOND_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+		
+		System.out.printf("Length of First Line is :: %.2f \n",LENGTH_OF_FIRST_LINE);
+		System.out.printf("Length of Second Line is :: %.2f \n",LENGTH_OF_SECOND_LINE);
+	
+		if (LENGTH_OF_FIRST_LINE == LENGTH_OF_SECOND_LINE) {
+			System.out.println("Lines Are Equal");
+		}
+		else {
+			System.out.println("Lines are not equal");
+		}
 	}
 
 }
