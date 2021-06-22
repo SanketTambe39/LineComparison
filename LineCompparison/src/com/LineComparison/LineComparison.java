91c406ee584178b40da831b0525a5187024e84ee
Sanket Tambe <53613233+SanketTambe39@users.noreply.github.com>
2021-06-22 18:21:55 +0530

    Update LineComparison.java

--- LineCompparison/src/com/LineComparison/LineComparison.java
+++ LineCompparison/src/com/LineComparison/LineComparison.java
@@ -6,24 +6,33 @@ public class LineComparison {
 		
 		int x1=3,x2=5,y1=2,y2=6;
 		
-		double LENGTH_OF_FIRST_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+		Double LENGTH_OF_FIRST_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
 
 		int x3=3,x4=5,y3=2,y4=6;
 		
-		double LENGTH_OF_SECOND_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+		Double LENGTH_OF_SECOND_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
 		
 		System.out.printf("Length of First Line is :: %.2f \n",LENGTH_OF_FIRST_LINE);
 		System.out.printf("Length of Second Line is :: %.2f \n",LENGTH_OF_SECOND_LINE);
 	
-		if (LENGTH_OF_FIRST_LINE == LENGTH_OF_SECOND_LINE) {
+		if (LENGTH_OF_SECOND_LINE.equals(LENGTH_OF_FIRST_LINE) ) {
 			System.out.println("Lines Are Equal");
-		}
-		else if(LENGTH_OF_FIRST_LINE > LENGTH_OF_SECOND_LINE) {
-			System.out.println(" First Line is Greater ");
 		}
 		else
 		{
 			System.out.println(" Second Line is Greater ");
+		}
+		
+		int compare = LENGTH_OF_FIRST_LINE.compareTo(LENGTH_OF_SECOND_LINE);
+		if ( compare == 0) {
+			System.out.println("The Two Line are equal");
+		}
+		else if(compare < 0){
+			System.out.println("Line 2 is greater than Line 1 ");
+		}
+		else
+		{
+			System.out.println("Line 1 is greater than Line 2");
 		}
 	}
 
