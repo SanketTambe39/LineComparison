1166a171a347167dc490a8c42d41a435d2f6cb01
Sanket Tambe <53613233+SanketTambe39@users.noreply.github.com>
2021-06-22 18:20:09 +0530

    Update LineComparison.java

--- LineCompparison/src/com/LineComparison/LineComparison.java
+++ LineCompparison/src/com/LineComparison/LineComparison.java
@@ -6,11 +6,11 @@ public class LineComparison {
 		
 		int x1=3,x2=5,y1=2,y2=6;
 		
-		double LENGTH_OF_FIRST_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+		Double LENGTH_OF_FIRST_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
 
 		int x3=3,x4=5,y3=2,y4=6;
 		
-		double LENGTH_OF_SECOND_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+		Double LENGTH_OF_SECOND_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
 		
 		System.out.printf("Length of First Line is :: %.2f \n",LENGTH_OF_FIRST_LINE);
 		System.out.printf("Length of Second Line is :: %.2f \n",LENGTH_OF_SECOND_LINE);
